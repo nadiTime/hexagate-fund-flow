f1d5872bc8c0e19d8206f54251ab784030f82a48
nadi <nadavcn@gmail.com>
2024-08-08 17:23:50 +0200

    use funds flow

--- webapp/src/app/dashboard/hooks/useFundsFlow.ts
+++ webapp/src/app/dashboard/hooks/useFundsFlow.ts
@@ -1,20 +1,69 @@
-import { ChainAddress } from '@/app/types/chainAddress';
-import { Edge } from 'reactflow';
-
+import { getFundingGraph } from "@/app/api/funding";
+import { ChainAddress } from "@/app/types/chainAddress";
+import { FundingResData } from "@/app/types/fundingRecord";
+import { getGroupLayout } from "@/app/utils/getGroupLayout";
+import { useQuery } from "@tanstack/react-query";
+import { Edge, Node } from "reactflow";
 
 type FundsFlowData = {
-    readonly isLoading?: boolean;
-    readonly nodes: Node[];
-    readonly edges: Edge[];
-}
+  readonly isLoading?: boolean;
+  isError?: boolean;
+  readonly nodes: Node[];
+  readonly edges: Edge[];
+};
+
+const parseFundingGraph = (
+  data: FundingResData
+): { nodes: Node[]; edges: Edge[] } => {
+  const nodes: Node[] = [];
+  const edges: Edge[] = [];
+  data.edges.forEach((edge) => {
+    let sourceNode = nodes.find((node) => node.id === edge.source.address);
+    if (!sourceNode) {
+      sourceNode = {
+        position: { x: 0, y: 0 },
+        id: edge.source.address,
+        data: { label: edge.source.address, ...edge.source },
+      };
+      nodes.push(sourceNode);
+    }
+
+    let targetNode = nodes.find((node) => node.id === edge.dest.address);
+    if (!targetNode) {
+      targetNode = {
+        position: { x: 0, y: 0 },
+        id: edge.dest.address,
+        data: { label: edge.dest.address, ...edge.dest },
+      };
+      nodes.push(targetNode);
+    }
+    edges.push({
+      id: `${sourceNode.id}-${targetNode.id}`,
+      source: sourceNode.id,
+      target: targetNode.id,
+    });
+  });
+
+  return { nodes, edges };
+};
 
 export const useFundsFlow = (sourceAddress: ChainAddress): FundsFlowData => {
-    // const graph = getGroupLayout(nodes, edges, 'LR');
+  const { isLoading, isError, data } = useQuery({
+    queryKey: ["funding"],
+    queryFn: async () => {
+      const data = await getFundingGraph(sourceAddress);
+      return parseFundingGraph(data);
+    },
+  });
 
-    // return {
-    //     isLoading,
-    //     funds,
-    //     nodes: graph.nodes,
-    //     edges,
-    // };
+  const { nodes, edges } = data || { nodes: [], edges: [] };
+
+  const graph = getGroupLayout(nodes, edges, "LR");
+
+  return {
+    isLoading,
+    isError,
+    nodes: graph.nodes,
+    edges,
+  };
 };
