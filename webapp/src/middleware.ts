4dc46c85da999dc5ccca0179fcd3c8c91eb81164
nadi <nadavcn@gmail.com>
2024-08-08 16:37:36 +0200

    set default redirect to dashboard page

--- webapp/src/middleware.ts
+++ webapp/src/middleware.ts
@@ -1,6 +1,8 @@
-import type { NextRequest } from 'next/server';
+import { NextResponse, type NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-    // app should redirect to /dashboard by default
-    return
+  // app should redirect to /dashboard by default
+  const { pathname } = request.nextUrl;
+  if (pathname === "/")
+    return NextResponse.redirect(new URL("/dashboard", request.url));
 }
