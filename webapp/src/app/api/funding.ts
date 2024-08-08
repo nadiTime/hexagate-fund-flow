b39ea2f812a443331f652b8520a34218bae4ac7b
nadi <nadavcn@gmail.com>
2024-08-08 16:50:06 +0200

    get funding graph

--- webapp/src/app/api/funding.ts
+++ webapp/src/app/api/funding.ts
@@ -1,9 +1,13 @@
 "use client";
 
-import { ChainAddress } from '@/app/types/chainAddress';
-import { FundingResData } from '@/app/types/fundingRecord';
+import { ChainAddress } from "@/app/types/chainAddress";
+import { FundingResData } from "@/app/types/fundingRecord";
 
-
-export const getFundingGraph = async (source: ChainAddress): Promise<FundingResData> => {
-
+export const getFundingGraph = async (
+  source: ChainAddress
+): Promise<FundingResData> => {
+  const res = await fetch(
+    `${process.env.API_SERVER}/api/v1/funding/graph/${source.chainId}/${source.address}`
+  );
+  return res.json();
 };
