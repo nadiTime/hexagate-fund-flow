2bbf1d47244ec0cbfecd7f9b44261912e3f599e7
nadi <nadavcn@gmail.com>
2024-08-08 18:42:33 +0200

    move server url to const

--- webapp/src/app/api/funding.ts
+++ webapp/src/app/api/funding.ts
@@ -1,5 +1,9 @@
 "use client";
 
+// Isnt api routes supposed to run on the server side?
+
+const API_SERVER = "http://localhost:3000";
+
 import { ChainAddress } from "@/app/types/chainAddress";
 import { FundingResData } from "@/app/types/fundingRecord";
 
@@ -7,7 +11,7 @@ export const getFundingGraph = async (
   source: ChainAddress
 ): Promise<FundingResData> => {
   const res = await fetch(
-    `http://localhost:3000/api/v1/funding/graph/${source.chainId}/${source.address}`
+    `${API_SERVER}/api/v1/funding/graph/${source.chainId}/${source.address}`
   );
   return res.json();
 };
