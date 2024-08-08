c2a518844a275dfe68a43cbeae7e0632153e8a0e
nadi <nadavcn@gmail.com>
2024-08-08 18:02:03 +0200

    page styles

--- webapp/src/app/dashboard/page.tsx
+++ webapp/src/app/dashboard/page.tsx
@@ -5,6 +5,7 @@ import { useFundsFlow } from "./hooks/useFundsFlow";
 import ReactFlow, { Background } from "reactflow";
 import { useMemo } from "react";
 import { FundNode } from "../components/charts";
+import { Spinner } from "../components/spinner/Spinner";
 
 const sourceChainAddress: ChainAddress = {
   address: "0x39cd23328b5ba304ae70bb0c1866e224f727f962",
@@ -16,18 +17,34 @@ export default function Dashboard() {
   const { isLoading, isError, nodes, edges } = useFundsFlow(sourceChainAddress);
   const nodeTypes = useMemo(() => ({ fundNode: FundNode }), []);
   return (
-    <div style={{ width: "100%", height: "100%", backgroundColor: "#F1F5F9" }}>
-      <div style={{ height: "100%" }}>
-        <ReactFlow
-          nodeTypes={nodeTypes}
-          nodes={nodes}
-          edges={edges}
-          draggable
-          nodesDraggable
-        >
-          <Background color="#F1F5F9" />
-        </ReactFlow>
-      </div>
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        backgroundColor: "#F1F5F9",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        color: "#000",
+      }}
+    >
+      {isLoading && <Spinner />}
+      {isError && <h1>Error...</h1>}
+      {!isLoading && nodes.length === 0 && <h1>No data...</h1>}
+      {nodes.length > 0 && (
+        <div style={{ height: "80%", width: "80%" }}>
+          <ReactFlow
+            fitView
+            nodeTypes={nodeTypes}
+            nodes={nodes}
+            edges={edges}
+            draggable
+            nodesDraggable
+          >
+            <Background color="#F1F5F9" />
+          </ReactFlow>
+        </div>
+      )}
     </div>
   );
 }
