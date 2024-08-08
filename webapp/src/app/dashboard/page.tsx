1a75680ab9adf246ba8f7a0985bf782641632290
nadi <nadavcn@gmail.com>
2024-08-08 17:36:44 +0200

    base page works

--- webapp/src/app/dashboard/page.tsx
+++ webapp/src/app/dashboard/page.tsx
@@ -1,13 +1,25 @@
-'use client';
+"use client";
 
-import { AddressType, ChainAddress, ChainIds } from '@/app/types/chainAddress';
+import { AddressType, ChainAddress, ChainIds } from "@/app/types/chainAddress";
+import { useFundsFlow } from "./hooks/useFundsFlow";
+import ReactFlow, { Background } from "reactflow";
 
 const sourceChainAddress: ChainAddress = {
-    address: '0x39cd23328b5ba304ae70bb0c1866e224f727f962',
-    chainId: ChainIds.EthereumMainnet,
-    type: AddressType.EOA,
+  address: "0x39cd23328b5ba304ae70bb0c1866e224f727f962",
+  chainId: ChainIds.EthereumMainnet,
+  type: AddressType.EOA,
 };
 
 export default function Dashboard() {
+  const { isLoading, isError, nodes, edges } = useFundsFlow(sourceChainAddress);
 
+  return (
+    <div style={{ width: "100%", height: "100%", backgroundColor: "#F1F5F9" }}>
+      <div style={{ height: "100%" }}>
+        <ReactFlow nodes={nodes} edges={edges} draggable nodesDraggable>
+          <Background color="#F1F5F9" />
+        </ReactFlow>
+      </div>
+    </div>
+  );
 }
